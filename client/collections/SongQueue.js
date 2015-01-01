c07b142f258a72185caadd5db7ed9ef988351b0a
dsernst <github@dsernst.com>
2014-12-31 17:05:15 -0800

    passes 22 out of 24 tests

--- client/collections/SongQueue.js
+++ client/collections/SongQueue.js
@@ -1,7 +1,27 @@
 // SongQueue.js - Defines a backbone model class for the song queue.
 var SongQueue = Songs.extend({
 
+  playFirst: function () {
+    this.at(0).play();
+  },
+
   initialize: function(){
+    this.on('add',function(){
+      if (this.length===1){
+        this.playFirst();
+      }
+    },this);
+
+    this.on('dequeue',function(){
+      this.shift();
+    },this);
+
+    this.on('ended',function() {
+      if (this.length > 0) {
+        this.playFirst();
+      }
+    }, this);
+
   }
 
 });
--- client/models/SongModel.js
+++ client/models/SongModel.js
@@ -1,9 +1,29 @@
 // SongModel.js - Defines a backbone model class for songs.
 var SongModel = Backbone.Model.extend({
 
+  initialize: function(){
+
+    this.on('ended',function(){
+      this.dequeue();
+    },this);
+  },
+
   play: function(){
     // Triggering an event here will also trigger the event on the collection
     this.trigger('play', this);
+  },
+
+  enqueue: function(){
+
+    this.trigger('enqueue', this);
+  },
+
+  dequeue: function(){
+    this.trigger('dequeue', this);
+  },
+
+  ended: function(){
+    this.trigger('ended', this);
   }
 
 });
